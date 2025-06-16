edfee277839a35737e336dc10c373e9eaee55d6f
mohit639592 <mohitsharma639592@gmail.com>
2025-06-16 19:04:45 +0530

    Add files via upload

---
+++ javascript/Basics_150625/basics.js
@@ -0,0 +1,28 @@
+// variable   var , let  , const
+ 
+//var
+
+var a = 23;
+console.log(a);
+a=32;
+console.log(a);
+var a = 20;        // we already initialize a but we are again intilize a and overwrite the old value of a that is bad thing in var
+console.log(a);
+
+//let
+
+let b = 24;
+console.log(b);
+b = 25;               // we can overwrite the variable in let
+console.log(b);  
+var b = 25;          // showing already been declared
+console.log(b);
+
+// const
+
+const a = 54;
+console.log(a);
+a=34;                    // showing error (Assignment to constant variable.) means we can not overwrite or update the vaiable in const
+console.log(a);
+
+
